83e92a1ffd07bb918f769dd999624e0b4c14388f
Nico <amicalazar@gmail.com>
2020-05-20 19:15:51 -0400

    Styled Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ContenedorHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
+
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0p;
+    font-family: 'Slabo 27 px', serif;
+    text-align: center;
+`;
+
 
 const Header = ({titulo}) => {
     return ( 
-        <header>
-            <h1>
+        <ContenedorHeader>
+            <TextoHeader>
                 {titulo}
-            </h1>
-        </header>
+            </TextoHeader>
+        </ContenedorHeader>
      );
 }
  
